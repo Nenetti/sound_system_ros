4d94d14ba03a8960d759292a5e6b099a2bc0fb5b
Nenetti <akinori1090@gmail.com>
2019-02-11 15:52:15 +0900

    通信部分完成

--- scripts/main.py
+++ scripts/main.py
@@ -3,39 +3,56 @@
 
 from module.julius import Julius
 from module.se import SE
+from module.svox import SVOX
 import rospy
+import rosparam
 from std_msgs.msg import String
 import subprocess
-
+import os
+import rospkg
 
 def start():
-    def resume(data):
+    def resume(message=None):
         # type: (String) -> None
-        module.resume()
-        print(SE.START)
-        SE.play(SE.START)
+        if not module.is_active:
+            module.resume()
+            SE.play(SE.START)
 
-
-    def pause(data):
+    def pause(message=None):
         # type: (String) -> None
-        module.pause()
-        SE.play(SE.STOP)
+        if module.is_active:
+            module.pause()
+            SE.play(SE.STOP)
 
-    rospy.init_node('voice_recognition', anonymous=False)
+    def speak(message):
+        # type: (String) -> None
+        pause()
+        SVOX.play(message.data)
+        resume()
+
+    rospy.init_node('sound_system', anonymous=False)
+    rospy.set_param("/pkg", rospkg.RosPack().get_path('sound_system'))
+
     rospy.Subscriber("sound_system/resume", String, resume)
     rospy.Subscriber("sound_system/pause", String, pause)
-    nlp_pub = rospy.Publisher('natural_language/text', String, queue_size=10)
+    rospy.Subscriber("sound_system/speak", String, speak)
+    reco_pub = rospy.Publisher('sound_system/recognition', String, queue_size=10)
 
-    module = Julius()
+    config = rospy.get_param("/sound_system/config")
+    host = rospy.get_param("/sound_system/host")
+    port = rospy.get_param("/sound_system/port")
+    is_debug = rospy.get_param("/sound_system/debug")
 
+    module = Julius(host, port, config, is_debug)
+    
     while True:
-        result = module.recognition()
-        if result is not None:
-            nlp_pub.publish(String(result))
-
-
-
-
+        if module.is_active:
+            result = module.recognition()
+            if result is not None:
+                result = result.replace("_", " ")
+                reco_pub.publish(String(result))
+                if is_debug:
+                    print("RESULT: %s" % result)
 
 
 if __name__ == '__main__':
