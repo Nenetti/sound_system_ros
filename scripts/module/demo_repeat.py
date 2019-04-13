fb3bde392263affbd93189b82da210b8f04e9836
Nenetti <akinori1090@gmail.com>
2019-04-13 10:31:22 +0900

    [update] 返答のメッセージを修正

--- scripts/module/demo_repeat.py
+++ scripts/module/demo_repeat.py
@@ -5,10 +5,11 @@ import rospy
 from std_msgs.msg import String
 
 if __name__ == "__main__":
-
     def subscribe(message):
         # type: (String) -> None
-        pub.publish(message.data)
+        text = "You said "+message.data
+        pub.publish(text)
+
 
     rospy.init_node('demo_repeat', anonymous=False)
     rospy.Subscriber('sound_system/recognition/result', String, subscribe)
